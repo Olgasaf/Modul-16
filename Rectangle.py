80ad0474d2853c01c06f5f2ebd085c39caaa9bdd
Olgasaf <101572462+Olgasaf@users.noreply.github.com>
2022-05-15 19:26:57 +0300

    Add files via upload

---
+++ Rectangle.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.heidht = height
+
+    def getWidth(self):
+        return self.width
+
+    def getHeidht(self):
+        return self.heidht
+
+    # Метод расчитывающий площадь
+    def getArea(self):
+        return self.width * self.heidht
+
+
+
