819961c7b02eb9f3317e0e60b1c92c0bead60f17
Olgasaf <101572462+Olgasaf@users.noreply.github.com>
2022-05-15 19:27:27 +0300

    Add files via upload

---
+++ testrectangle.py
@@ -0,0 +1,9 @@
+from Rectangle import Rectangle
+
+r1 = Rectangle(10,5)
+
+print("r1.width=", r1.width)
+print("r1.heidht=", r1.heidht)
+print("r1.getWidth=",r1.getWidth())
+print("r1.gerArea=", r1.getArea())
+
